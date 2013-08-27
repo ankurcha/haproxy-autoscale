44b41ea046c88ba1d88a3430106407f6e13c1186
Osvaldo Mena <osvaldo.mena@pyro.cl>
2013-08-26 17:47:16 -0700

    License Changes! =)

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 setup(name='haproxy-update',
       version='1.0',
       description='HAProxy wrapper for handling auto-scaling EC2 instances.',
-      author='Seahorse Backend Team',
-      author_email='ignacio@seahorse.co',
-      url='https://github.com/buenaonda/haproxy-update',
+      author='Mark Caudill',
+      author_email='mark@markcaudill.me',
+      url='https://github.com/markcaudill/haproxy-update',
       install_requires=['boto>=2.0', 'mako>=0.5.0', 'argparse>=1.2.1'],
-      license='JAJAJAJA'                                                    )
+      license='MIT'                                                          )
